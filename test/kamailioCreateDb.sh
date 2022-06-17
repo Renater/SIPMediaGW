461c40ad628cdb278198ba1645bdf9e93bd9197e
Nicolas Tizon <nicolas.tizon@gmail.com>
2022-06-17 17:36:06 +0200

    [Changed] gateway locking mechanism

--- test/kamailioCreateDb.sh
+++ test/kamailioCreateDb.sh
@@ -2,4 +2,4 @@
 
 touch `pwd`/kamailio.sqlite
 docker run -it -v `pwd`/kamailio.sqlite:/usr/local/etc/kamailio/kamailio.sqlite -v `pwd`/kamailio:/etc/kamailio:ro --entrypoint kamdbctl  kamailio  create
-docker run -it -v `pwd`/kamailio.sqlite:/usr/local/etc/kamailio/kamailio.sqlite -v `pwd`/kamailio:/etc/kamailio:ro --entrypoint  sqlite3  kamailio /usr/local/etc/kamailio/kamailio.sqlite "alter table location add column last_locked TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT '2000-01-01 00:00:01'"
+docker run -it -v `pwd`/kamailio.sqlite:/usr/local/etc/kamailio/kamailio.sqlite -v `pwd`/kamailio:/etc/kamailio:ro --entrypoint  sqlite3  kamailio /usr/local/etc/kamailio/kamailio.sqlite "alter table location add column locked INTEGER NOT NULL DEFAULT 0"
