74730a90d2f86e865fb82be76bf45b98d88cb697
Nicolas Tizon <nicolas.tizon@gmail.com>
2022-10-05 14:03:12 +0200

    [Added] database logger in kamailio

---
+++ test/kamailioGetLogs.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import sqlite3
+import configparser
+
+dbPath = '/usr/local/etc/kamailio/kamailio.sqlite'
+congiFile = "/etc/sipmediagw.cfg"
+
+config = configparser.ConfigParser()
+config.read(congiFile)
+gwNamePart = config['mediagw']['sipUaNamePart'].replace('"',"").replace("'", "")
+
+dbCon = sqlite3.connect(dbPath)
+
+with dbCon as con:
+    cursor = con.cursor()
+
+    cursor.execute('''SELECT contact, username FROM location
+                      WHERE
+                          username LIKE '%'||?||'%'
+                          ;''',(gwNamePart,))
+    contactList = cursor.fetchall()
+    print('Number of registered SIPMediaGWs: {}'.format(len(contactList)), flush=True)
+
+    cursor.execute('''SELECT contact, username FROM location
+                      WHERE
+                          locked = 1 AND
+                          username LIKE '%'||?||'%'
+                          ;''',(gwNamePart,))
+    contactList = cursor.fetchall()
+    print('Number of busy SIPMediaGWs: {}'.format(len(contactList)), flush=True)
+
+    cursor.execute('''SELECT contact, username FROM location
+                      WHERE
+                          locked = 0 AND
+                          username LIKE '%'||?||'%' AND
+                          NOT EXISTS (
+                             SELECT callee_contact
+                             FROM dialog
+                             WHERE callee_contact LIKE '%'||location.contact||'%'
+                          );''',(gwNamePart,))
+    contactList = cursor.fetchall()
+    print('Number of available SIPMediaGWs: {}'.format(len(contactList)), flush=True)
+
+
