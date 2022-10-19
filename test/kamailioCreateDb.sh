eec395066e40c77b1e7162041c8227af54f67c10
Nicolas Tizon <nicolas.tizon@gmail.com>
2022-10-19 14:13:37 +0200

    [Changed] kamailio database creation and running

--- test/kamailioCreateDb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-touch `pwd`/kamailio.sqlite
-docker run -it -v `pwd`/kamailio.sqlite:/usr/local/etc/kamailio/kamailio.sqlite -v `pwd`/kamailio:/etc/kamailio:ro --entrypoint kamdbctl  kamailio4sipmediagw  create
-docker run -it -v `pwd`/kamailio.sqlite:/usr/local/etc/kamailio/kamailio.sqlite -v `pwd`/kamailio:/etc/kamailio:ro --entrypoint  sqlite3  kamailio4sipmediagw /usr/local/etc/kamailio/kamailio.sqlite "alter table location add column locked INTEGER NOT NULL DEFAULT 0"
--- test/kamailioRun.py
+++ test/kamailioRun.py
@@ -1,22 +1,48 @@
 #!/usr/bin/env python
+import os
 import subprocess
+import sqlite3
+from contextlib import closing
+
+dbPath = '/usr/local/etc/kamailio/kamailio.sqlite'
+
+def unlock(dstUser):
+    with closing(sqlite3.connect(dbPath)) as con:
+        with closing(con.cursor()) as cursor:
+            cursor.execute('''UPDATE location SET locked = 0
+                              WHERE  contact LIKE '%'||?||'%';''',
+                           (dstUser,))
+            res = con.commit()
+
+with open(dbPath, 'a'):
+    try:
+        subprocess.run(['kamdbctl create'], shell=True)
+        with closing(sqlite3.connect(dbPath)) as con:
+            with closing(con.cursor()) as cursor:
+                cursor.execute('''ALTER TABLE location
+                                  ADD COLUMN locked INTEGER NOT NULL DEFAULT 0''')
+                res = con.commit()
+    except:
+        pass
 
 cmd = subprocess.Popen('kamailio -DD -E', stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 
 while True:
-    line = cmd.stdout.readline()
-    if line:
-        logs = line.decode()
-        try:
-            if logs.find('ACC: call missed:') != -1:
-                accLog = logs.split('ACC: call missed:')[1]
-                accDict = dict(el.split("=") for el in accLog.split(";"))
-                if accDict['code'] == '500':
-                    print('Unregister locked gateway: ',accDict['dst_user'], '\n', flush=True)
-                    userName = accDict['dst_user'].rsplit('-0x', 1)[0]
-                    subprocess.run(['kamctl ul rm {}'.format(userName)], shell=True)
-        except:
-            print('ACC logs parsing error \n', flush=True)
-        print(logs, end='', flush=True)
+    try:
+        line = cmd.stdout.readline()
+        if line:
+            logs = line.decode()
+            try:
+                if logs.find('ACC: call missed:') != -1:
+                    accLog = logs.split('ACC: call missed:')[1]
+                    accDict = dict(el.split("=") for el in accLog.split(";"))
+                    if accDict['code'] == '500':
+                        print('Unlock gateway: ',accDict['dst_user'], '\n', flush=True)
+                        unlock(accDict['dst_user'])
+            except:
+                print('ACC logs parsing error \n', flush=True)
+            print(logs, end='', flush=True)
+    except:
+        pass
 
 
