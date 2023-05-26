fd83e29f973c33bbcac063568ca2c30d1f21704c
Nicolas Tizon <nicolas.tizon@gmail.com>
2023-05-26 17:04:48 +0200

    [Added] IP configuration with NAT

--- test/coturn/coturnRun.py
+++ test/coturn/coturnRun.py
@@ -6,7 +6,7 @@ coturnRunCmd='turnserver --log-file=stdout --lt-cred-mech \
                          --min-port=40000 --max-port=49999'
 
 if os.getenv("STUN_SRV"):
-    coturnRunCmd+= ' --external-ip={}'.format(os.getenv("STUN_SRV"))
+    coturnRunCmd+= ' --external-ip={}/{}'.format(os.getenv("STUN_SRV"),os.getenv("LOCAL_IP"))
 
 coturnRunCmd+=' --user={}:{}'.format(os.getenv("STUN_USER"), os.getenv("STUN_PASS"))
 
