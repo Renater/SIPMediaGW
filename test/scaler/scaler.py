bba9269d4b6a4f54a4e445f0a2d1e189346cb9f0
Nicolas Tizon <nicolas.tizon@gmail.com>
2023-10-13 10:16:05 +0200

    [Fixed] scaling logic

--- test/scaler/scaler.py
+++ test/scaler/scaler.py
@@ -52,46 +52,50 @@ def upScale(csp, numCPU):
     if numCPU != 0:
         csp.createInstance(cpu, gwNamePart)
 
-
 def downScale(csp, numGW):
+    SQLCheck='''SELECT id
+                    FROM location AS loc2
+                    WHERE loc1.vm =
+                        SUBSTRING_INDEX(SUBSTRING_INDEX(loc2.received,'sip:',-1),':',1) AND
+                        loc2.locked = 1'''
+    SQLCheckBis='''SELECT  callee_contact
+                    FROM dialog
+                    WHERE loc1.vm =
+                        SUBSTRING_INDEX(SUBSTRING_INDEX(callee_contact,'alias=',-1),'~',1)'''
     try:
         with closing(mysqlcon.connect(host=kamctlrc['DBHOST'],
                                       database='kamailio',
                                       user='root',
                                       password=kamctlrc['DBROOTPW'])) as con:
             with closing(con.cursor(dictionary=True)) as cursor:
-                cursor.execute('''SELECT SUBSTRING_INDEX(SUBSTRING_INDEX(received,'sip:',-1),':',1) AS vm,
-                                         COUNT(username) as count FROM location
-                                WHERE
-                                    locked = 0 AND
-                                    username LIKE CONCAT('%',%s,'%') AND
-                                    NOT EXISTS (
-                                        SELECT  callee_contact
-                                        FROM dialog
-                                        WHERE SUBSTRING_INDEX(SUBSTRING_INDEX(callee_contact,'alias=',-1),'~',1) =
-                                              SUBSTRING_INDEX(SUBSTRING_INDEX(location.received,'sip:',-1),':',1)
-                                    )
-                                GROUP BY vm
-                                ORDER BY count DESC;''',(gwNamePart,))
+                cursor.execute('''SELECT vm, COUNT(username) as count
+                                  FROM
+                                      (SELECT *,
+                                           SUBSTRING_INDEX(SUBSTRING_INDEX(received,'sip:',-1),':',1) AS vm
+                                               FROM location) AS loc1
+                                  WHERE
+                                      loc1.locked = 0 AND
+                                      loc1.username LIKE CONCAT('%',%s,'%') AND
+                                      NOT EXISTS ('''+SQLCheck+''') AND
+                                      NOT EXISTS ('''+SQLCheckBis+''')
+                                  GROUP BY vm
+                                  ORDER BY count DESC;''',(gwNamePart,))
                 vmList = cursor.fetchall()
 
                 ipList = []
                 for vm in vmList:
                     if vm['count'] <= numGW:
                         cursor.execute('''UPDATE location SET locked = 1
-                                        WHERE
-                                            locked = 0 AND
-                                            SUBSTRING_INDEX(SUBSTRING_INDEX(location.received,'sip:',-1),':',1)=%s AND
-                                            NOT EXISTS (
-                                                SELECT  callee_contact
-                                                FROM dialog
-                                                WHERE SUBSTRING_INDEX(SUBSTRING_INDEX(callee_contact,'alias=',-1),'~',1) =
-                                                      SUBSTRING_INDEX(SUBSTRING_INDEX(location.received,'sip:',-1),':',1)
-                                            )''',(vm['vm'],))
-                        res = con.commit()
+                                          WHERE
+                                              locked = 0 AND
+                                              SUBSTRING_INDEX(SUBSTRING_INDEX(location.received,'sip:',-1),':',1)=%s''',
+                                        (vm['vm'],))
                         if cursor.rowcount == vm['count']:
+                            con.commit()
                             ipList.append(vm['vm'])
                             numGW -= vm['count']
+                        else:
+                            con.rollback()
 
     except mysqlcon.Error as err:
         print("Mysql error: {}".format(err), flush=True)
@@ -126,8 +130,10 @@ def scale(csp):
 
                 if currentCapacity < thresholdTimeLine[th]['capaMin']:
                     upScale(csp, (thresholdTimeLine[th]['capaMin'] - currentCapacity)*cpuPerGW)
+                    currentCapacity = thresholdTimeLine[th]['capaMin']
 
-                capacityIncrease = math.ceil(inCallNum/thresholdTimeLine[th]['loadMax'] - currentCapacity)
+                targetCapacity = max(thresholdTimeLine[th]['capaMin'], inCallNum/thresholdTimeLine[th]['loadMax'])
+                capacityIncrease = math.ceil(targetCapacity - currentCapacity)
                 if capacityIncrease > 0:
                     # Upscale
                     upScale(csp, capacityIncrease*cpuPerGW)
