ccf3288af9c4a6a405b1e1f4dd81be58eea6bd58
Nicolas Tizon <nicolas.tizon@gmail.com>
2023-10-17 16:29:04 +0200

    [Fixed] scaler implementation

--- test/scaler/scaler.py
+++ test/scaler/scaler.py
@@ -9,38 +9,11 @@ import inspect
 from contextlib import closing
 import mysql.connector as mysqlcon
 
-cspName = "outscale"
-configFile = "config_sample.json"
-
-thresholdTimeLine = {'00:00:00': {'capaMin':1, 'loadMax':0.90}, 
-                     '09:00:00': {'capaMin':4, 'loadMax':0.75},
-                     '12:00:00': {'capaMin':2, 'loadMax':0.80},
-                     '14:00:00': {'capaMin':4, 'loadMax':0.75},
-                     '17:00:00': {'capaMin':1, 'loadMax':0.90}}
-
-# Get a manageInstance object from CSP file name
-sys.path.append(cspName)
-modName = cspName
-print("CSP mod name: "+modName, flush=True)
-mod = importlib.import_module(modName)
-isClassMember = lambda member: inspect.isclass(member) and member.__module__ == modName
-cspObj = inspect.getmembers(mod, isClassMember)[0][1]
-
-csp = cspObj()
-csp.configureInstance("{}/{}".format(cspName, configFile))
-
-kamctlrc = dict()
-kamctlrc['DBHOST'] = os.getenv('MYSQL_HOST')
-kamctlrc['DBROOTPW'] = os.getenv('MYSQL_ROOT_PASSWORD')
-cpuPerGW = os.getenv('CPU_PER_GW')
-
-gwNamePart = os.environ.get('GW_NAME_PREFIX').replace('"',"").replace("'", "")
-
 def getSeconds(stringHMS):
    timedeltaObj = dt.datetime.strptime(stringHMS, "%H:%M:%S") - dt.datetime(1900,1,1)
    return timedeltaObj.total_seconds()
 
-def upScale(csp, numCPU):
+def upScale(csp, params, numCPU):
     # cpuRange values must be multiples of 4
     cpuRange = list(csp.instType.keys())
     cpuRange.sort(reverse=True)
@@ -48,11 +21,11 @@ def upScale(csp, numCPU):
         instNum = numCPU//int(cpu)
         numCPU = numCPU%int(cpu)
         for i in range(instNum):
-            csp.createInstance(cpu, gwNamePart)
+            csp.createInstance(cpu, params['gwNamePart'])
     if numCPU != 0:
-        csp.createInstance(cpu, gwNamePart)
+        csp.createInstance(cpu, params['gwNamePart'])
 
-def downScale(csp, numGW):
+def downScale(csp, params, numGW):
     SQLCheck='''SELECT id
                     FROM location AS loc2
                     WHERE loc1.vm =
@@ -63,10 +36,10 @@ def downScale(csp, numGW):
                     WHERE loc1.vm =
                         SUBSTRING_INDEX(SUBSTRING_INDEX(callee_contact,'alias=',-1),'~',1)'''
     try:
-        with closing(mysqlcon.connect(host=kamctlrc['DBHOST'],
+        with closing(mysqlcon.connect(host=params['DBHOST'],
                                       database='kamailio',
                                       user='root',
-                                      password=kamctlrc['DBROOTPW'])) as con:
+                                      password=params['DBROOTPW'])) as con:
             with closing(con.cursor(dictionary=True)) as cursor:
                 cursor.execute('''SELECT vm, COUNT(username) as count
                                   FROM
@@ -79,13 +52,13 @@ def downScale(csp, numGW):
                                       NOT EXISTS ('''+SQLCheck+''') AND
                                       NOT EXISTS ('''+SQLCheckBis+''')
                                   GROUP BY vm
-                                  ORDER BY count DESC;''',(gwNamePart,))
+                                  ORDER BY count DESC;''',(params['gwNamePart'],))
                 vmList = cursor.fetchall()
 
                 ipList = []
                 for vm in vmList:
                     if vm['count'] <= numGW:
-                        cursor.execute('''UPDATE location SET locked = 1
+                        cursor.execute('''UPDATE location SET locked = 1, to_stop = 1
                                           WHERE
                                               locked = 0 AND
                                               SUBSTRING_INDEX(SUBSTRING_INDEX(location.received,'sip:',-1),':',1)=%s''',
@@ -103,43 +76,67 @@ def downScale(csp, numGW):
     if ipList:
         csp.destroyInstances(ipList)
 
-def scale(csp):
-    currTime = dt.datetime.now().strftime("%H:%M:%S")
-    th = min([ i for i in list(thresholdTimeLine.keys()) if i < currTime],
-             key=lambda x:abs(getSeconds(x)-getSeconds(currTime)))
-
+def cleanup(csp, params):
     try:
-        with closing(mysqlcon.connect(host=kamctlrc['DBHOST'],
+        with closing(mysqlcon.connect(host=params['DBHOST'],
                                       database='kamailio',
                                       user='root',
-                                      password=kamctlrc['DBROOTPW'])) as con:
+                                      password=params['DBROOTPW'])) as con:
+            with closing(con.cursor(dictionary=True)) as cursor:
+                cursor.execute('''SELECT SUBSTRING_INDEX(SUBSTRING_INDEX(received,'sip:',-1),':',1) AS vm
+                                  FROM location
+                                  WHERE
+                                      username LIKE CONCAT('%',%s,'%') AND to_stop = 1
+                                  GROUP BY vm;''',(params['gwNamePart'],))
+                vmList = cursor.fetchall()
+    except mysqlcon.Error as err:
+        print("Mysql error: {}".format(err), flush=True)
+
+    ipList = []
+    for vm in vmList:
+        ipList.append(vm['vm'])
+    if ipList:
+        csp.destroyInstances(ipList)
+
+def scale(csp, params, thresholdTimeLine, scaleTime=None):
+    if not scaleTime:
+        scaleTime = dt.datetime.now().strftime("%H:%M:%S")
+    th = min([ i for i in list(thresholdTimeLine.keys()) if i < scaleTime],
+             key=lambda x:abs(getSeconds(x)-getSeconds(scaleTime)))
+
+    try:
+        with closing(mysqlcon.connect(host=params['DBHOST'],
+                                      database='kamailio',
+                                      user='root',
+                                      password=params['DBROOTPW'])) as con:
             with closing(con.cursor()) as cursor:
                 cursor.execute('''SELECT contact, username FROM location
                                 WHERE
-                                    username LIKE CONCAT('%',%s,'%')
-                                    ;''',(gwNamePart,))
+                                    username LIKE CONCAT('%',%s,'%') AND
+                                    to_stop = 0
+                                    ;''',(params['gwNamePart'],))
                 contactList = cursor.fetchall()
                 currentCapacity = len(contactList)
 
                 cursor.execute('''SELECT callee_contact FROM dialog
                                 WHERE
                                     callee_contact LIKE CONCAT('%',%s,'%')
-                                    ;''',(gwNamePart,))
+                                    ;''',(params['gwNamePart'],))
                 contactList = cursor.fetchall()
                 inCallNum = len(contactList)
 
                 if currentCapacity < thresholdTimeLine[th]['capaMin']:
-                    upScale(csp, (thresholdTimeLine[th]['capaMin'] - currentCapacity)*cpuPerGW)
+                    upScale(csp, params, (thresholdTimeLine[th]['capaMin'] - currentCapacity)*params['cpuPerGW'])
                     currentCapacity = thresholdTimeLine[th]['capaMin']
 
                 targetCapacity = max(thresholdTimeLine[th]['capaMin'], inCallNum/thresholdTimeLine[th]['loadMax'])
                 capacityIncrease = math.ceil(targetCapacity - currentCapacity)
                 if capacityIncrease > 0:
                     # Upscale
-                    upScale(csp, capacityIncrease*cpuPerGW)
+                    upScale(csp, capacityIncrease*params['cpuPerGW'])
                 if capacityIncrease < 0:
                     # Downscale
-                    downScale(csp, abs(capacityIncrease))
+                    downScale(csp, params, abs(capacityIncrease))
             
     except mysqlcon.Error as err:
         print("Mysql error: {}".format(err), flush=True)
