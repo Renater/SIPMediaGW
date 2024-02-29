5045788820c4d3c35353479e5b82b1efc1522e1e
Nicolas Tizon <nicolas.tizon@gmail.com>
2024-02-29 19:07:56 +0100

    [Fixed] audio driver configuration and v4l2 device check

--- streaming/streaming.sh
+++ streaming/streaming.sh
@@ -2,7 +2,15 @@
 
 ### Start default video capture ###
 ffmpeg  -f lavfi -i "smptebars=s=640x360" -vf drawtext="fontsize=60:text='STREAMING':x=(w-text_w)/2:y=(h-text_h)/2:fontcolor=White" -pix_fmt yuv420p \
-       -video_size 640x360 -f v4l2 /dev/video0 -loglevel error | logParse -p "Event" &
+       -video_size 640x360 -f v4l2 /dev/video0 -nostats 2> >( tee $STATE | logParse -p "Event") &
+
+if [ "$WITH_ALSA" == "true" ]; then
+    AUDRIVE='alsa'
+    AUDEV=$ALSA_DEV
+else
+    AUDRIVE='pulse'
+    AUDEV="VirtMicSrc1"
+fi
 
 ffmpeg \
       -hide_banner \
@@ -13,10 +21,10 @@ ffmpeg \
       -video_size $VID_SIZE_APP \
       -thread_queue_size 1024 \
       -i :$SERVERNUM0 \
-      -f pulse \
+      -f $AUDRIVE \
       -ac 2 \
       -thread_queue_size 1024 \
-      -i VirtMicSrc1 \
+      -i $AUDEV \
       -map 0:v:0 \
       -c:v libx264 \
       -preset ultrafast \
@@ -32,4 +40,9 @@ ffmpeg \
       -ar 44100 \
       -copyts \
       -f flv \
-      $RTMP_DST_URI &
+      $RTMP_DST_URI 1> >( logParse -p "Event") \
+                    2> >( logParse -p "Event") &
+
+### Event server ###
+exec python3 src/eventServer.py 1> >( logParse -p "Event") \
+                                2> >( logParse -p "Event") &
