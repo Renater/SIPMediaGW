13d0e3abb3f31e900e4ae4a3aa5f13905d963d73
Nicolas Tizon <nicolas.tizon@gmail.com>
2024-02-29 20:05:33 +0100

    [Changed] split HTTPLauncher and sipmediagw APIs

--- HTTPLauncher.py
+++ HTTPLauncher.py
@@ -47,24 +47,8 @@ class Launcher:
             web.ctx.status = '401 Unauthorized'
             return
 
-class Chat:
-    def POST(self):
-        data = json.loads(web.data().decode("utf-8"))#web.input()#data().decode("utf-8")
-        if 'room' in data:
-            room = data['room']
-        if 'msg' in data:
-            msg = data['msg']
-        msgDict = '{{"event":"message", "type":"CHAT_INPUT", "text": "{}" }}'.format(msg)
-        msgDict = msgDict.replace('\n', '\\n')
-        subprocess.run(['echo "{}" | netcat -q 1 {} 4444'.format(msgDict, room)],
-             shell=True)
-
-urls = "/(.*)", "Launcher"
+urls = ("/launcher", "Launcher")
 application = web.application(urls, globals()).wsgifunc()
-
-urls = ("/sipmediagw", "Launcher",
-    "/chat", "Chat")
-
 
 app = web.application(urls, globals())
 if __name__ == "__main__":
