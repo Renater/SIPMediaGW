1a6daccda9eb43b0fe061dfba00ea484d8343823
Nicolas Tizon <nicolas.tizon@gmail.com>
2025-10-02 18:40:52 +0200

    [Fixed] screen sharing button id

--- browsing/assets/teams.js
+++ browsing/assets/teams.js
@@ -63,7 +63,7 @@ class Teams {
         if (key == "5")
             document.querySelector('button[id="roster-button"]').click();
         if (key == "s")
-            document.querySelector('button[id="share-button"]').click();
+            document.querySelector('button#share-button, button#screenshare-button').click();
             var logOutBtn = await this.waitForElement('[data-tid="share-screen-window-or-tab"]', { clickable: true }, 10000);
             logOutBtn.click();
     }
